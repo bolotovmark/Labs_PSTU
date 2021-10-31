7d0f7fe2204309cf7a02c128b29be5596636b03e
Mark Bolotov <bolmark2003@gmail.com>
2021-10-31 10:29:45 +0500

    1v 04.11.21

---
+++ 04.11.21/task2/task2.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Cake on 31.10.2021.
+//
+// 1 куб фут = 7.481 галона
+#include <iostream>
+
+using namespace std;
+
+float x;
+const float i = 7.481;
+
+int main(){
+    cin>>x;
+    cout<<endl<<x/i<<endl;
+}
---
+++ 04.11.21/task3/task3.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Cake on 31.10.2021.
+//
+//
+// Created by Cake on 31.10.2021.
+//
+#include <iostream>
+
+using namespace std;
+
+const int a = 10;
+int b;
+int main(){
+    cout<<a<<endl;
+
+    b = 20;
+    cout<<b<<endl;
+
+    cout<<--b<<endl;
+
+    return 0;}
+
