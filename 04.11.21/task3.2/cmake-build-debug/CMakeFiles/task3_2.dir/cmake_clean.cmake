ee7e70c7537de6a99559f8f520ea707a3f36bcb3
Mark Bolotov <bolmark2003@gmail.com>
2021-11-04 13:01:45 +0500

    add task3.2 and update task3.1

---
+++ 04.11.21/task3.2/cmake-build-debug/CMakeFiles/task3_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task3_2.dir/task3.2.cpp.obj"
+  "libtask3_2.dll.a"
+  "task3_2.exe"
+  "task3_2.exe.manifest"
+  "task3_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task3_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
